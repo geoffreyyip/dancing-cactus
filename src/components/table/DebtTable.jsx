6d73411eb813a5fb8f76c318c4f1e539317eb2c8
imagiraffe123 <gyip4@binghamton.edu>
2017-01-01 12:36:03 -0500

    refactored data table settings settings will now be part of each line item component as opposed to being part of the main data table component more modular this way

--- src/components/table/DebtTable.jsx
+++ src/components/table/DebtTable.jsx
@@ -1,15 +1,9 @@
 import React from 'react';
-import ModificationBar from './ModificationBar';
-import AdditionBar from './AdditionBar';
-import SaveChangesBar from './SaveChangesBar';
 import DisplayLineItemContainer from '../../containers/table/DisplayLineItemContainer';
 
 const DebtTable = () => (
   <div className="debt-table">
     <DisplayLineItemContainer />
-    <ModificationBar />
-    <AdditionBar />
-    <SaveChangesBar />
   </div>
 );
 
