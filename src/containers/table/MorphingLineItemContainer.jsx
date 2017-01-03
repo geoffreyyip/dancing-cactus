0fb46b3990f3706fbcbeef5257de7eac81369c29
imagiraffe123 <gyip4@binghamton.edu>
2017-01-03 14:52:00 -0500

    Remove dead code from morphing components

--- src/containers/table/MorphingLineItemContainer.jsx
+++ src/containers/table/MorphingLineItemContainer.jsx
@@ -1,10 +1,6 @@
 import React from 'react';
-// import MorphingLineItem from '../../components/table/MorphingLineItem';
 
 const MorphingLineItemContainer = props => (
-  // <MorphingLineItem>
-  //   {props.children}
-  // </MorphingLineItem>
   <div>
     {props.children}
   </div>
@@ -13,32 +9,5 @@ const MorphingLineItemContainer = props => (
 MorphingLineItemContainer.propTypes = {
   children: React.PropTypes.arrayOf(React.PropTypes.node),
 };
-
-/*
-MorphingLineItemContainer.propTypes = {
-  name: React.PropTypes.string.isRequired,
-  amount: React.PropTypes.number.isRequired,
-  interestRate: React.PropTypes.number.isRequired,
-  minPayment: React.PropTypes.number.isRequired,
-  handleDeleteChanges: React.PropTypes.func.isRequired,
-};
-*/
-/*
-class MorphingLineItemContainer extends React.Component {
-  constructor(props) {
-    super(props);
-
-  }
-
-  render() {
-    return (
-      <MorphingLineItem
-        name={mockItem.name}
-        amount={mockItem.amount}
-        interestRate={mockItem.interestRate}
-        minPayment={mockItem.minPayment} />
-    );
-  }
-}*/
 
 export default MorphingLineItemContainer;
