db0fec593b1222ec2b1879f5d693bddc896947d4
imagiraffe123 <gyip4@binghamton.edu>
2017-01-13 12:33:04 -0500

    Add payment schedule calculator
    Optionally allow user to specify an accelerator amount (what they
    would like to pay on top of their minimum monthly payments) and a
    payment method (snowball means apply accelerator to the smallest
    balance loan first and avalanche means apply to highest interest
    first).
    
    Set accelerator default to 0 and payment method default to snowball.
    
    Accept an array of debt objects and output that same array but
    with a schedule property defined on each debt. Schedule property
    is an array of monthly payments objects, each of which specifies
    the total monthly payment, the payment towards interest, the
    payment towards principal reduction, and the amount leftover at the
    end.

---
+++ src/helpers/paymentSchedule.js
@@ -0,0 +1,172 @@
+// TODO: split this documentation across functions.
+
+/** 
+ * Calculate payment schedules given a list of debts.
+ *
+ * Input should be an array of objects with properties of "name", "amount", 
+ * "interestRate", and "minPayment".
+ *
+ * For example:
+ *   [
+ *    { name: 'Car Loan', amount: 9000, interestRate: 0.05, minPayment: 620.23 },
+ *    { name: 'College', amount: 20000, interestRate: 0.07, minPayment: 1685.47 },
+ *    { name: 'Mortgage', amount: 43000, interestRate: 0.035, minPayment: 2100.00 },
+ *   ]
+ *
+ * A payment schedule is generated per loan. paymentSchedule should be an array of
+ * objects with properties of "payment", "principal", "interest",
+ * "balance". A line item is generated for each month until the leftover balance
+ * hits zero
+ *
+ * "payment" means total payment that month
+ * "principal" means principal paid that month
+ * "interest" means interest paid that month
+ * "leftover" means leftover balance after payment
+ *
+ * For example:
+ *   [
+ *     { payment: 620.23, principal: 585.31, interest: 34.92, leftover: 7794.46 },
+ *     { payment: 620.23, principal: 587.75, interest: 32.48, leftover: 7206.71 },
+ *     ...
+ *     { payment: 620.23, principal: 620.23, interest: 10.07, leftover: 1805.51 },
+ *     { payment: 1813.03, principal: 1805.51, interest: 7.52, leftover: 0 },
+ *   ]
+ * 
+ * Default payment schedule assumes an accelerator of 0. In other words, it assumes
+ * the user is paying only the minimum amounts in month 1, and repeating that
+ * payment for each month after. (Note that monthly payments per loan may not remain
+ * static. If a different debt gets paid off, any monthly payments from that loan
+ * will automatically get applied to a user's other loans.)
+ * 
+ * If an accelerator is specified, that amount is applied to the debt with the
+ * highest priority. If user has chosen the snowball method that means the debt with
+ * the smallest leftover amount. If the user has chosen the 'avalanche' method, then
+ * that means the debt with the highest interest rate.
+ *
+ * All payment schedules will be rolled up into a paymentSchedules object linking
+ * each loan to a paymentSchedule array. This represents the final output.
+ */
+
+const SNOWBALL = 'snowball';
+const AVALANCHE = 'avalanche';
+
+// SNOWBALL means pay debts from smallest balance to largest
+// AVALANCHE means pay debts from highest interest to lowest
+const prioritize = function(debts, payoffMethod = SNOWBALL) {
+  // shallow copy to ensure immutability of arguments
+  const dues = debts.slice();
+
+  const compareByAmount = (curr, next) => (curr.amount - next.amount);
+  const compareByInterest = (curr, next) => (curr.interestRate - next.interestRate);
+
+  if (payoffMethod === SNOWBALL) {
+    dues.sort(compareByAmount);
+  } else if (payoffMethod === AVALANCHE) {
+    dues.slice().reverse().sort(compareByInterest);
+  } else {
+    throw Error('Prioritize receieved invalid payOffMethod');
+  }
+
+  return dues;
+}
+
+/**
+ * A payment schedule is generated per loan. paymentSchedule should be an array of
+ * objects with properties of "payment", "principal", "interest",
+ * "balance". A line item is generated for each month until the leftover balance
+ * hits zero
+ *
+ * "payment" means total payment that month
+ * "principal" means principal paid that month
+ * "interest" means interest paid that month
+ * "leftover" means leftover balance after payment
+ *
+ * For example:
+ *   [
+ *     { payment: 620.23, principal: 585.31, interest: 34.92, leftover: 7794.46 },
+ *     { payment: 620.23, principal: 587.75, interest: 32.48, leftover: 7206.71 },
+ *     ...
+ *     { payment: 620.23, principal: 620.23, interest: 10.07, leftover: 1805.51 },
+ *     { payment: 1813.03, principal: 1805.51, interest: 7.52, leftover: 0 },
+ *   ]
+ * End example.   
+ */
+const payMandatoryFees = function(leftover, minPayment, rate) {
+  const result = {};
+  result.payment = minPayment;
+
+  const interest = leftover * rate;
+  const principalReduction = minPayment - interest;
+  const remainingBalance = leftover - principalReduction;
+  
+  result.interest = interest;
+  result.principal = principalReduction;
+  result.leftover = remainingBalance;
+
+  return result;
+}
+
+/** 
+ * NOTE: a negative balance may exist in the month's payment,
+ * this means that the mandatory monthly payment exceeded the
+ * remaining balance. That surplus payment gets applied 
+ * towards the accelerator for future debts.
+ */
+const payExtra = function(month, accelerator) {
+  const result = { ...month };  // shallow copy object
+  let extra = accelerator;
+
+  if (extra < month.leftover) {
+    result.principal += extra;            // increases payment by extra
+    result.leftover -= extra;             // reduce balance by extra
+    extra = 0;                            // set extra to 0 for this cycle
+  } else {
+    result.principal += result.leftover;  // pay off remaining balance
+    extra -= result.leftover;             // subtract from leftover
+    result.leftover = 0;                  // set remaining balance to 0
+  }
+
+  return { extra: extra, currMonth: result };
+}
+
+const last = function(arr) {
+  const lastIndex = arr.length - 1;
+  return arr[lastIndex];
+}
+
+// if a debt schedule does not exist, assign amount to leftover and
+// assign blank array to debt.schedule
+const getLeftover = function(debt) {
+  if (debt.schedule.length === 0) {
+    return debt.amount;
+  }
+  return last(debt.schedule).leftover;
+}
+
+const addSchedule = function(debt) {
+  return { ...debt, schedule: [] };
+};
+
+const getPaymentSchedule = function(debts, accelerator = 0, payoffMethod) {
+  const dues = prioritize(debts, payoffMethod).map(addSchedule);
+
+  // repeat until all debts hit zero
+  do {
+    let extra = accelerator;
+
+    dues.forEach((debt) => {
+      const monthlyFee = debt.minPayment;
+      const rate = debt.interestRate;
+      const leftover = getLeftover(debt);
+
+      let currMonth = payMandatoryFees(leftover, monthlyFee, rate);
+      ( {extra, currMonth} = payExtra(currMonth, extra) );
+
+      debt.schedule.push(currMonth);
+    });
+  } while (last(last(dues).schedule).leftover > 0.0001);
+
+  return dues;
+};
+
+export { prioritize, payMandatoryFees, getPaymentSchedule, payExtra };
---
+++ src/helpers/paymentSchedule.test.js
@@ -0,0 +1,124 @@
+import chai, { expect } from 'chai';
+import chaiSorted from 'chai-sorted';
+import {
+  prioritize,
+  payMandatoryFees,
+  getPaymentSchedule,
+  payExtra,
+} from './paymentSchedule.js';
+
+
+chai.use(chaiSorted);
+
+describe('prioritize', () => {
+  let SNOWBALL;
+  let AVALANCHE;
+  let sampleDebts;
+
+  beforeEach(() => {
+    SNOWBALL = 'snowball';
+    AVALANCHE = 'avalanche';
+
+    sampleDebts = [
+    { name: 'College Loan', amount: 20000, interestRate: 0.07, minPayment: 1685.47 },
+    { name: 'Car Loan', amount: 9000, interestRate: 0.05, minPayment: 620.23 },
+    { name: 'Mortgage', amount: 43000, interestRate: 0.035, minPayment: 2100.00 },
+    ];
+  });
+
+  it('can sort debts in ascending amount order', () => {
+    const orderedDebts = prioritize(sampleDebts, SNOWBALL);
+    expect(orderedDebts).to.be.ascendingBy('amount');
+  });
+
+  it('can sort debts in descending interestRate order', () => {
+    const orderedDebts = prioritize(sampleDebts, AVALANCHE);
+    expect(orderedDebts).to.be.descendingBy('interestRate');
+  });
+});
+
+describe('getPaymentSchedule', () => {
+  let sampleDebts;
+
+  beforeEach(() => {
+    sampleDebts = [
+    { name: 'College Loan', amount: 20000, interestRate: 0.07, minPayment: 1685.47 },
+    { name: 'Car Loan', amount: 9000, interestRate: 0.05, minPayment: 620.23 },
+    { name: 'Mortgage', amount: 43000, interestRate: 0.035, minPayment: 2100.00 },
+    ];
+  });
+
+  it('should map its input to an output of equivalent length', () => {
+    const inputLen = sampleDebts.length;
+    expect(getPaymentSchedule(sampleDebts)).to.have.lengthOf(inputLen);
+  });
+
+  it('should return the right properties and types', () => {
+    const debts = getPaymentSchedule(sampleDebts);
+    const debt = debts[0];
+
+    expect(debt.name).to.be.a('string');
+    expect(debt.amount).to.be.a('number');
+    expect(debt.interestRate).to.be.a('number');
+    expect(debt.minPayment).to.be.a('number');
+    expect(debt.schedule).to.be.instanceof(Array);
+  });
+
+  it('should reduce all debts to zero', () => {
+    const debts = getPaymentSchedule(sampleDebts);
+    debts.forEach((debt) => {
+      expect(debt.schedule.pop().leftover).to.be.closeTo(0, 0.00001);
+    });
+  });
+
+  it('should reduce debts over time', () => {
+    const debts = getPaymentSchedule(sampleDebts);
+    const debt = debts[0];
+    expect(debt.schedule).to.be.descendingBy('leftover');
+  });
+
+  /**
+   * TODO: Write test to detect ascending then descending pattern
+   * pass: [2, 4, 5, 10, 2, 0, 0, 0]
+   * fail: [2, 4, 5, 10, 2, 0, 0, 8]
+   */
+  /**
+  it('should pay a higher principal over time then flatten out', () => {
+    const debts = getPaymentSchedule(sampleDebts);
+    const debt = debts[0];
+
+    // iterate over array until first decrease is spotted
+    const dip = debt.forEach
+
+    expect(debt.schedule).to.be.ascendingBy('principal');
+  });
+   */
+
+  it('should pay lower interest over time', () => {
+    const debts = getPaymentSchedule(sampleDebts);
+    const debt = debts[0];
+    expect(debt.schedule).to.be.descendingBy('interest'); 
+  });
+});
+
+describe('payMandatoryFees', () => {
+  let leftover;
+  let currPayment;
+  let interestRate;
+
+  beforeEach(() => {
+    currPayment = 1685.47;
+    interestRate = 0.07; 
+    leftover = 15148.05;
+  });
+
+  // smoke test
+  it('returns the right properties and types', () => {
+    const nextMonth = payMandatoryFees(leftover, currPayment, interestRate);
+
+    expect(nextMonth.payment).to.be.a('number');
+    expect(nextMonth.interest).to.be.a('number');
+    expect(nextMonth.principal).to.be.a('number');
+    expect(nextMonth.leftover).to.be.a('number');
+  });
+});
