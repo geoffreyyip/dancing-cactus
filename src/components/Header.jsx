2fa7d64dfd25ffab010d1960f9ea84be73391a2d
imagiraffe123 <gyip4@binghamton.edu>
2017-01-19 15:11:51 -0500

    Forgot to track Header component in previous commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => (
+  <div className="Header">
+    <div className="Header-title">
+      Debt Payoff Calculator
+    </div>
+    <div className="Header-subtitle">
+      See how long it takes to pay off your debts!
+    </div>
+  </div>
+);
+
+export default Header;
